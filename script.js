9ace1120a11283450995af1c3f4772ef4c789de4
Miguel Miyaki da Cruz - SENAI <miguel.m.cruz6@aluno.senai.br>
2026-04-10 09:43:00 -0300

    Refactor game initialization and event listeners

--- script.js
+++ script.js
@@ -6,67 +6,41 @@ const tabuleiro = document.getElementById("tabuleiro")
 
 
 facil.addEventListener("click", () =>{
-    const blocos = document.querySelectorAll(".bloco")
-    blocos.forEach(bloco => bloco.remove())
-    iniciar(3, 3, 165)
+    tabuleiro.style.gridTemplateColumns = 'repeat(3, 1fr)'; /* 3 colunas iguais */
+    iniciar(3, 165)
 })
 
 medio.addEventListener("click", () =>{
-    const blocos = document.querySelectorAll(".bloco")
-    blocos.forEach(bloco => bloco.remove())
-    iniciar(4, 4, 123.5)
+    tabuleiro.style.gridTemplateColumns = 'repeat(4, 1fr)';   /* 4 colunas iguais */
+    iniciar(4, 123.5)
 })
 
 dificil.addEventListener("click", () =>{
-    const blocos = document.querySelectorAll(".bloco")
-    blocos.forEach(bloco => bloco.remove())
-    iniciar(5, 5, 98.9)
+    tabuleiro.style.gridTemplateColumns = 'repeat(5, 1fr)';   /* 5 colunas iguais */
+    iniciar(5, 98.9)
 })
 
 
-
-
-
-
-function iniciar(colunas, linhas, tamanho){
-    let contador = 1
-    for(let y = 0; y < colunas; y++){
-        for(let x = 0; x < linhas; x++){
-            const div = document.createElement("div")
-            div.innerHTML = `<div>${contador}</div>`
-            div.classList.add("bloco")
-            div.style.width = `${tamanho}px`
-            div.style.height = `${tamanho}px`
-            tabuleiro.appendChild(div)
-            contador++
-        }
-    }
-    embaralhar(colunas, linhas)
-}
-
-
-function embaralhar(array){
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1))
-        ;[array[i], array[j]] = [array[j], array[i]]
-    }
-}
-function iniciar(colunas, linhas, tamanho, a){
+function iniciar(espaco, tamanho){
+    tabuleiro.innerHTML = ""                                  // Deleta os blocos
     let valores = []
-
-    for (let i = 1; i <= colunas * linhas; i++) {
+    for (let i = 1; i <= espaco * espaco; i++) {
         valores.push(i)
     }
 
     embaralhar(valores)
 
     let index = 0
-
-    for(let y = 0; y < colunas; y++){
-        for(let x = 0; x < linhas; x++){
+    for(let y = 0; y < espaco; y++){
+        for(let x = 0; x < espaco; x++){
             const div = document.createElement("div")
-            div.innerHTML = `<div>${valores[index]}</div>`
-            div.classList.add("bloco")
+            if(valores[index] === espaco * espaco){
+                div.innerHTML = `<div></div>`
+                div.classList.add("vazio")
+            } else{
+                div.innerHTML = `<div>${valores[index]}</div>`
+                div.classList.add("bloco")
+            }
             div.style.width = `${tamanho}px`
             div.style.height = `${tamanho}px`
             tabuleiro.appendChild(div)
@@ -75,6 +49,14 @@ function iniciar(colunas, linhas, tamanho, a){
     }
 }
 
+
+
+function embaralhar(array){
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1))
+        ;[array[i], array[j]] = [array[j], array[i]]
+    }
+}
 
 
 
