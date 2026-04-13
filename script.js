5985d996c0430fc4026fab81d48b6a5cc30e5160
Miguel Miyaki da Cruz - SENAI <miguel.m.cruz6@aluno.senai.br>
2026-04-12 21:28:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,24 +5,24 @@ const dificil = document.getElementById("dificil")
 const tabuleiro = document.getElementById("tabuleiro")
 
 
-facil.addEventListener("click", () =>{
-    tabuleiro.style.gridTemplateColumns = 'repeat(3, 1fr)'; /* 3 colunas iguais */
+facil.addEventListener("click", () => {
+    tabuleiro.style.gridTemplateColumns = 'repeat(3, 1fr)';   /* 3 colunas iguais */
     iniciar(3, 165)
 })
 
-medio.addEventListener("click", () =>{
+medio.addEventListener("click", () => {
     tabuleiro.style.gridTemplateColumns = 'repeat(4, 1fr)';   /* 4 colunas iguais */
     iniciar(4, 123.5)
 })
 
-dificil.addEventListener("click", () =>{
+dificil.addEventListener("click", () => {
     tabuleiro.style.gridTemplateColumns = 'repeat(5, 1fr)';   /* 5 colunas iguais */
     iniciar(5, 98.9)
 })
 
 
-function iniciar(espaco, tamanho){
-    tabuleiro.innerHTML = ""                                  // Deleta os blocos
+function iniciar(espaco, tamanho) {
+    tabuleiro.innerHTML = ""                                   // Deleta os blocos
     let valores = []
     for (let i = 1; i <= espaco * espaco; i++) {
         valores.push(i)
@@ -31,13 +31,13 @@ function iniciar(espaco, tamanho){
     embaralhar(valores)
 
     let index = 0
-    for(let y = 0; y < espaco; y++){
-        for(let x = 0; x < espaco; x++){
+    for (let y = 0; y < espaco; y++) {
+        for (let x = 0; x < espaco; x++) {
             const div = document.createElement("div")
-            if(valores[index] === espaco * espaco){
+            if (valores[index] === espaco * espaco) {
                 div.innerHTML = `<div></div>`
                 div.classList.add("vazio")
-            } else{
+            } else {
                 div.innerHTML = `<div>${valores[index]}</div>`
                 div.classList.add("bloco")
             }
@@ -49,15 +49,32 @@ function iniciar(espaco, tamanho){
     }
 }
 
-
-
-function embaralhar(array){
+function embaralhar(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1))
-        ;[array[i], array[j]] = [array[j], array[i]]
+            ;[array[i], array[j]] = [array[j], array[i]]
     }
 }
 
 
+
+tabuleiro.addEventListener("click", (e) => {
+    const bloco = e.target.closest(".bloco")
+    if (!bloco) return
+    const vazio = document.querySelector(".vazio")
+    const blocoRect = bloco.getBoundingClientRect()
+    const vazioRect = vazio.getBoundingClientRect()
+    const dx = Math.abs(blocoRect.left - vazioRect.left)
+    const dy = Math.abs(blocoRect.top - vazioRect.top)
+    if ((dx === 0 && dy === blocoRect.height) || (dy === 0 && dx === blocoRect.width)) {
+        const temp = bloco.innerHTML
+        bloco.innerHTML = vazio.innerHTML
+        vazio.innerHTML = temp
+        bloco.classList.remove("bloco")
+        bloco.classList.add("vazio")
+        vazio.classList.remove("vazio")
+        vazio.classList.add("bloco")
+    }
+})
 
 
